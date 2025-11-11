ea1321866a46957d3d17d148d2085d558f4caa04
YarasaniNikhil123 <Yarasaninikhil@wealthzonegroupai.com>
2025-11-11 17:32:31 +0530

    first commit

---
+++ otr-admin/src/services/endpoints.ts
@@ -0,0 +1,27 @@
+
+
+const endpoints = {
+
+    login: 'api/auth/login',
+    register: 'api/auth/register',
+    sendEmailOtp: 'api/auth/send-email-otp',
+    verifyEmailOtp: "api/auth/verify-email-otp",
+    sendMobileOtp: "api/auth/send-mobile-otp",
+    verifyMobileOtp: "api/auth/verify-mobile-otp",
+    Syllabus: 'api/syllabus/upload',
+    uploadPQP: 'api/pqp/pqpupload',
+    uploadAnswer: 'api/answerkey/uploadbyAnswer',
+    uploadResult: 'api/result/Resultupload',
+    uploadCutoff: 'api/cutoff/upload',
+    createJobpost: 'jobpost/create',
+    adminCard: 'api/admit-card/generate-by-job?jobPostId=',
+
+    // 
+    createQuestionpapers:'api/question-paper/create?jobPostId=',
+    getCategories: "jobpost/categories",
+    examRelease: "examAssignment/assign?jobPostId=",
+
+
+}
+
+export default endpoints
